7453c91463bd8ccc0cc8894d43f2a929ccf84db3
Leandro Costa <leandrin.costta@gmail.com>
2022-06-16 16:34:13 -0300

    Desafio Funcao

---
+++ script.js
@@ -0,0 +1,33 @@
+//Exercicio 1
+function mensagem(){
+    console.log("Olá Mundo")
+}
+mensagem()
+
+// Exercicio 2
+
+function MeuNome (nome){
+    console.log("Meu nome é:" ,nome)
+}
+MeuNome("Leandro");
+
+// Exercicio 3
+
+function informacao (nome,idade,estiloMusical){
+    console.log(nome,idade,estiloMusical)
+} 
+informacao("Leandro","28 anos","Todos os ritmos")
+
+// Exercicio 4
+
+function sobre (filme,musica){
+    console.log(filme,musica)
+}
+sobre("Terror","Todas")
+
+// Exercicio Bônus
+
+function numero (n1){
+    return n1 *3
+}
+console.log("O valor multplicado por 3 é", numero(20));
